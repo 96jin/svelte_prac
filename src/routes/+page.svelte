062e034f39f8a3a20044645eb79cacde02eb7bfe
96jin <gjwlsgur123@gmail.com>
2023-02-15 17:53:24 +0900

    Html Structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // @ts-ignore
+  import TodoItem from '$lib/todo-item.svelte'
+  // 경로 설정할 때 $ 표시로 해도 된다.
+
+  const title = "Todos | New"
+</script>
+
+<svelte:head>
+  <title>{title} | New</title>
+</svelte:head>
+
+<h1>{title}</h1>
+
+<form action="" method="" >
+  <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo">
+</form>
+
+<TodoItem/>
