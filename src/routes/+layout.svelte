4241d613b4b19f435eee3e2f0e8ac975ee842f2c
96jin <gjwlsgur123@gmail.com>
2023-02-16 17:31:40 +0900

    Apply Css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<!-- 글로벌로 스타일을 입히고 싶을때, routes 디렉토리에 +layout.svelte 생성 후 css import 한다. 그리고 slot 태그 -->
+<script>
+  import "../app.css"
+</script>
+
+<main>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/another">another</a>
+  </nav>
+  <slot />
+</main>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,2 @@
 <!-- about 폴더를 만들어주고 +page.svelte 파일을 만들어 라우팅한다. -->
-<h1>About us</h1>
+<h1>About page</h1>
